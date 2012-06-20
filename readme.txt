310a7cb39126e86c6be7ae481e7dfd64fe970395
kobenland <kobenland@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-20 16:40:24 +0000

    Version 1.3.0
    git-svn-id: https://plugins.svn.wordpress.org/wp-google-suggest/trunk@561208 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: kobenland
 Tags: search, AJAX, jQuery, Google, Google suggest
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=LYYF6RDJ5A2EA
 Requires at least: 3.3
-Tested up to: 3.3.1
-Stable tag: 1.2
+Tested up to: 3.4
+Stable tag: 1.3.0
 
 Provides suggestions while typing a search query, using the Google Suggest API.
 
@@ -14,7 +14,7 @@ This plugin uses an abstraction of jQueryUI's autocomplete widget that interacts
 Props go to Haochi Chen.
 
 It adds an AJAX call to the search form, returning matches for the current search query.
-There is no change of template files necessary as this plugin hooks in the existing WordPress API to unfold its magic.
+Make sure the searchform input field has `id="s"` applied, so this plugin can hook in the existing WordPress API to unfold its magic.
 
 
 == Installation ==
@@ -22,14 +22,20 @@ There is no change of template files necessary as this plugin hooks in the exist
 1. Download WP Google Suggest.
 2. Unzip the folder into the `/wp-content/plugins/` directory
 3. Activate the plugin through the 'Plugins' menu in WordPress
+4. Make sure the searchform input field has `id="s"`
 
 
 == Frequently Asked Questions ==
 
-None asked yet.
+= Why are there no suggestions, when I enter a searchterm? =
+To pick up the user entry, the Plugin needs the search input field to have `id="s"`, like in the default Themes that come with WordPress.
 
 
 == Changelog ==
+
+= 1.3.0 =
+* Updated Utility Class
+* Tested with WordPress 3.4
 
 = 1.2 =
 * Fixed a bug that prevented the plugin from activating
--- wp-google-suggest.php
+++ wp-google-suggest.php
@@ -2,23 +2,23 @@
 /** wp-google-suggest.php
  *
  * Plugin Name:	WP Google Suggest
- * Plugin URI:	http://www.obenlands.de/en/2011/05/wp-google-suggest/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-google-suggest
+ * Plugin URI:	http://en.wp.obenland.it/wp-google-suggest/#utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-google-suggest
  * Description:	Uses the Google Suggest API to suggest related search terms
- * Version:		1.2
+ * Version:		1.3.0
  * Author:		Konstantin Obenland
- * Author URI:	http://www.obenlands.de/en/?utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-google-suggest
+ * Author URI:	http://en.wp.obenland.it/#utm_source=wordpress&utm_medium=plugin&utm_campaign=wp-google-suggest
  * Text Domain: wp-google-suggest
  * Domain Path: /lang
  * License:		GPLv2
  */
 
 
-if( ! class_exists('Obenland_Wp_Plugins') ) {
-	require_once('obenland-wp-plugins.php');
+if ( ! class_exists( 'Obenland_Wp_Plugins_v300' ) ) {
+	require_once( 'obenland-wp-plugins.php' );
 }
 
 
-class Obenland_Wp_Google_Suggest extends Obenland_Wp_Plugins {
+class Obenland_Wp_Google_Suggest extends Obenland_Wp_Plugins_v300 {
 	
 	
 	///////////////////////////////////////////////////////////////////////////
@@ -38,25 +38,14 @@ class Obenland_Wp_Google_Suggest extends Obenland_Wp_Plugins {
 
 		parent::__construct( array(
 			'textdomain'		=>	'wp-google-suggest',
-			'plugin_name'		=>	plugin_basename(__FILE__),
+			'plugin_path'		=>	__FILE__,
 			'donate_link_id'	=>	'LYYF6RDJ5A2EA'
 		));
 		
 		if ( ! is_admin() ) {
-			add_filter( 'init', array(
-				&$this,
-				'register_scripts_styles'
-			), 9); // Set to 9, so they can easily be deregistered
-				
-			add_filter( 'wp_print_scripts', array(
-				&$this,
-				'print_scripts'
-			));
-			
-			add_filter( 'wp_print_styles', array(
-				&$this,
-				'print_styles'
-			));
+			$this->hook( 'init', 9 ); // Set to 9, so they can easily be deregistered
+			$this->hook( 'wp_print_scripts' );
+			$this->hook( 'wp_print_styles' );
 		}
 	}
 	
@@ -75,20 +64,20 @@ class Obenland_Wp_Google_Suggest extends Obenland_Wp_Plugins {
 	 *
 	 * @return	void
 	 */
-	public function register_scripts_styles() {
+	public function init() {
 		$suffix = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '.dev' : '';
 
 		wp_register_script(
 			$this->textdomain,
-			plugins_url("/js/wp-google-suggest$suffix.js", __FILE__),
-			array('jquery-ui-autocomplete'),
+			plugins_url( "/js/{$this->textdomain}{$suffix}.js", __FILE__ ),
+			array( 'jquery-ui-autocomplete' ),
 			false,
 			true
 		);
 		
 		wp_register_style(
 			$this->textdomain,
-			plugins_url("/css/wp-google-suggest$suffix.css", __FILE__),
+			plugins_url( "/css/{$this->textdomain}{$suffix}.css", __FILE__ ),
 			array(),
 			false
 		);
@@ -104,7 +93,7 @@ class Obenland_Wp_Google_Suggest extends Obenland_Wp_Plugins {
 	 *
 	 * @return	void
 	 */
-	public function print_scripts() {
+	public function wp_print_scripts() {
 		wp_enqueue_script( $this->textdomain );
 	}
 	
@@ -118,7 +107,7 @@ class Obenland_Wp_Google_Suggest extends Obenland_Wp_Plugins {
 	 *
 	 * @return	void
 	 */
-	public function print_styles() {
+	public function wp_print_styles() {
 		wp_enqueue_style( $this->textdomain );
 	}
 
